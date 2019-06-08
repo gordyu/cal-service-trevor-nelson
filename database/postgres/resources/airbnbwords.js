392bc36af15fb7c1fbed1324fdb876be2395f7c3
trevjnels <trevorjohnnels@gmail.com>
2019-06-07 20:39:27 -0600

    Seeding function is working now in terms of making the text file. Need to fill out object with faker info and then attach it to the database

---
+++ database/postgres/resources/airbnbwords.js
@@ -0,0 +1,64 @@
+
+
+const homes = [
+  'underwater dome',
+  'yurt',
+  'garage',
+  'puebla',
+  'living-room space',
+  'infinity pool',
+  'horse-and-carriage',
+  'backyard',
+  'regrigerator box',
+  'communal living space',
+  'co-op',
+  'lightly-used sofa',
+  'home',
+  'mansion',
+  'penthouse suite',
+  'apartment',
+  'hyperbolic chamber',
+  'duplex',
+  'condo',
+  'guest house'
+];
+
+const proximity = [
+  'in',
+  'around',
+  'by',
+  'next to',
+  'located in',
+  'in the heart of',
+  'in the middle of',
+  'located near',
+  'within a stone\'s throw of',
+  'in walking distance of',
+  'near',
+  'close to',
+  'underneath'
+];
+
+const nearby = [
+  'a busy cross-section',
+  'outerspace',
+  'underwater',
+  'downtown',
+  'the countryside',
+  'the Upside-Down',
+  'the shadowrealm',
+  'a virtual world',
+  'an alternate reality',
+  'my grandmother\'s house',
+  'the woods',
+  'Hogwart\'s',
+  'Candyland',
+  'the Wall',
+  'the Red Keep',
+  'the public park',
+  'the Cow level',
+  'Kanto',
+  'Mars'
+];
+
+module.exports = { nearby, proximity, homes}
--- database/postgres/seed.js
+++ database/postgres/seed.js
@@ -1,59 +1,81 @@
 const fs = require('fs')
 const path = require('path')
 const {AsyncParser} = require('json2csv')
+const faker = require('faker')
 
 const db = require("./db.js")
-const seedFilePath = "./SeedSilo/seedFile.csv"
+const words = require("./resources/airbnbwords.js")
+const seedFilePath = "./SeedSilo/seedFile.txt"
 
-//setting up async parser
-// const feilds = ["listing_name", "host_name", "max_guests", "listing_price"]
-// const opts = { feilds }
-// const transformOpts = {highWaterMark: 8192}
-// const asyncParser = new AsyncParser(opts, transformOpts)
-// let csv = ''
-//- - - - - - - 
+var homesLength = words.homes.length
+var proximityLength = words.proximity.length
+var nearbyLength = words.nearby.length
+var headers = "listing_name\thost_name\tmax_guests\tlisting_price\n"
 
-
-// asyncParser.processor
-// 	.on('data', chunk => {
-// 		return (csv += chunk.toString())
-// 	})
-// 	.on('end', ()=> {
-// 		console.log('------------------------------------');
-// 	 fs.writeFile(path.join(__dirname, seedFilePath), csv,'utf8', ()=> {console.log('doneWriting CSV file')})
-// 		console.log('------------------------------------');
-// 	})
-// 	.on('error', err => console.error(err))
-
-
-
-	var seed = {
-		"listing_name": "Trevors treehouse of magic vacations",
-		"host_name": "James Franko",
-		"max_guests": 24,
-		"listing_price": 239
+// var numSeeds = 10000000
+var numSeeds = 100
+const objCreate = (i) => {
+	var seedObj = {
+		"listing_name": `" ${faker.name.findName() + faker.name.findName() + faker.name.findName() + faker.name.findName() + faker.name.findName()}"`,
+		"host_name": "Brandy Nelson",
+		"max_guests": 4,
+		"listing_price": 209
 		}
-		// {
-		// 	"listing_name": "the FOREST cabin",
-		// 	"host_name": "Brandy Nelson",
-		// 	"max_guests": 4,
-		// 	"listing_price": 209
-		// 	}]
-var string2
-var string = `listing_name","host_name","max_guests","listing_price"\n`
-		// seed = JSON.stringify(seed)
-		for(let i = 0; i<5000000; i++){
-			string += `"Trevors treehouse of magic vacations","James Franko",24,239\n`
-		}
-		for(let i = 0; i<5000000; i++){
-			string2 += `"listing_name","host_name","max_guests","listing_price""Trevors treehouse of magic vacations","James Franko",24,239\n`
-		}
+		return seedObj;
+}
+// console.log(objCreate())
 	
-		fs.writeFile(path.join(__dirname, seedFilePath), (string + string2),'utf8', (err )=> {
-			if(err) console.error(err)
-			console.log('wrote first half of csv')
-			console.log('done!')
-		})
+
+
+fs.writeFile(path.join(__dirname ,seedFilePath), headers, 'utf8', (err, data) => {
+	if(err) console.error(err)
+	else {
+	fs.open(path.join(__dirname ,seedFilePath), 'r+', (err, fd) => {
+		if(err) console.error(err)
+		else {
+		console.log('------------------------------------');
+		console.log('Opened file @' + Date());
+		console.log('------------------------------------');
+		
+		const recurser = (n) => {
+			if(n>numSeeds) {
+				console.log('------$-------------$----------$-------');
+				console.log('FileWrite complete @' + Date());
+				console.log('--$-----------$-------------$---------$-');
+				return;
+			}
+			var {listing_name, host_name, max_guests, listing_price} = objCreate();
+			var inputString = `${listing_name}\t${host_name}\t${max_guests}\t${listing_price}\n`
+			
+			fs.write(fd, inputString, (err) => {
+				if(err) console.error(err)
+				recurser(n+1)
+			})
+		}
+		recurser(0)
+		}
+			
+	})
+}
+})
+
+
+
+		// seed = JSON.stringify(seed)
+
+		// for(let i = 0; i<9000000; i++){
+			
+		
+		// 			string += `"${seedObj['listing_name']}","james franko",24,239\n`
+		// }
+		// for(let i = 0; i<5; i++){
+		// 	string2 += `"Trevors treehouse of magic vacations","James Franko",24,239\n`
+		// }
+	
+		// fs.writeFile(path.join(__dirname, seedFilePath), (string + string2),'utf8', (err )=> {
+		// 	if(err) console.error(err)
+		// 	console.log('DONEZO!')
+		// })
 	
 			// asyncParser.input.push(string)
 		
@@ -64,21 +86,11 @@ var string = `listing_name","host_name","max_guests","listing_price"\n`
 		// }
 		// 	asyncParser.input.push(string2)
 	
-		console.log('------------------------------------');
-		// console.log(string.length);
-		console.log('------------------------------------');
-		// asyncParser.input.push(null);
 
 
 
 
-// const seedFile = require(seedFilePath)
 
-
-
-
-// fs.writeFileSync(path.join(__dirname, seedFilePath), csv ,'utf8')
-
-
-
-// console.log(JSON.stringify(seed)) 
+	// string += `"BEAUTIFUL ${words.homes[i%homesLength] +" "+ words.proximity[i%proximityLength] +" "+ words.nearby[i%nearbyLength] }","james franko",24,239\n`
+			// string += `"Trevors treehouse of magic vacations","James Franko",24,239\n`
+				// string += `"BEAUTIFUL ${words.homes[i%homesLength] +" "+ words.proximity[i%proximityLength] +" "+ words.nearby[i%nearbyLength] }","james franko",24,239\n`
