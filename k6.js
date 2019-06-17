91db97492c2f939a0f85a014de439883c77ba0ef
trevjnels <trevorjohnnels@gmail.com>
2019-06-17 13:57:00 -0600

    Repeat after me: I WILL ALWAYS commit AND *PUSH* my work.

---
+++ k6.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+	vus      : 10,
+	duration : '5s'
+};
+
+export default function() {
+	let randomID = 900000 + Math.floor(Math.random() * 99000);
+	let res = http.get(`http://localhost:3002/${randomID}`);
+	check(res, {
+		sucess : (r) => r.status == 200
+	});
+}
